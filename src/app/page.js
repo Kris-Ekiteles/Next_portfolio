2a3085a15722ac1981e3d97a01651bdacf93e187
kris <krisekteles@gmail.com>
2025-08-23 18:04:06 +0300

    new div for first sec and another for the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,15 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h3>Hello, I'm Kris</h3>
-      <p>full stack developer $ UX/UI enthusiast</p>
-      <button className="btn">view my work</button>
+    <div className="main-page">
+      <div className="view">
+        <h3>Hello, I'm Kris</h3>
+        <p>full stack developer $ UX/UI enthusiast</p>
+        <button className="btn">view my work</button>
+      </div>
+      <div className="hero">
+        <h3>this is the hero section</h3>
+      </div>
     </div>
   );
 }
