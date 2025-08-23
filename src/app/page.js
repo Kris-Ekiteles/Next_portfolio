da9f3e2e632f61343045206021a17364e703abdb
kris <krisekteles@gmail.com>
2025-08-23 23:41:21 +0300

    using the card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
     <div className="main-page">
       <div className="view">
+        
         <h3>Hello, I'm Kris</h3>
         <p>full stack developer $ UX/UI enthusiast</p>
         <button className="btn">view my work</button>
@@ -14,6 +16,11 @@ export default function Home() {
         <p>
           A MERN stack developer passionate about building full-cycle applications. I bridge the gap between front-end elegance and back-end robustness, delivering seamless and dynamic user experiences.
         </p>
+      </div>
+      <hr></hr>
+      <div className="projects">
+        
+       <Card />
       </div>
     </div>
   );
