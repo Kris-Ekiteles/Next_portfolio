a0fcfc39a1377209b56f5ecd9514f0a5b624a5c8
kris <krisekteles@gmail.com>
2025-08-25 23:53:17 +0300

    contact form

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { FaFacebook, FaWhatsapp, FaGithub } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="contact-me">
+        <input type="email" placeholder="send an email" />
+        <button>send</button>
+        <BsFillTelephoneOutboundFill />
+      </div>
       <div className="social-media">
         <FaFacebook />
         <BsTwitterX />
