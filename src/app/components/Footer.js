afa138ebe078561b20341e9409b47e69e0f3257a
kris <krisekteles@gmail.com>
2025-08-25 23:44:25 +0300

    social media icons

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FaFacebook, FaWhatsapp, FaGithub } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>
-      footer side
+    <div className="footer">
+      <div className="social-media">
+        <FaFacebook />
+        <BsTwitterX />
+        <FaWhatsapp />
+        <FaGithub />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
